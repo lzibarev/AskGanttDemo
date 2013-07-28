ae14765ec52bd284f0659332685006edb7fcd340
lzibarev <lzibarev@yandex.ru>
2013-07-28 15:51:59 +0400

    Логотип для папки.

--- docroot/WEB-INF/src/ru/ask/primaview/gantt/demo/client/bacisgantt/PrimaveraGantt.java
+++ docroot/WEB-INF/src/ru/ask/primaview/gantt/demo/client/bacisgantt/PrimaveraGantt.java
@@ -18,9 +18,16 @@ import com.gantt.client.Gantt;
 import com.gantt.client.config.GanttConfig;
 import com.google.gwt.cell.client.DateCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.dom.client.Style.Overflow;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.scheduler.client.core.TimeResolution.Unit;
@@ -33,6 +40,9 @@ import com.scheduler.client.core.timeaxis.preconfig.YearTimeAxisGenerator;
 import com.scheduler.client.zone.Line;
 import com.scheduler.client.zone.LineProperties;
 import com.scheduler.client.zone.ZoneGeneratorInt;
+import com.sencha.gxt.core.client.dom.DomHelper;
+import com.sencha.gxt.core.client.dom.XDOM;
+import com.sencha.gxt.core.client.dom.XElement;
 import com.sencha.gxt.core.client.resources.StyleInjectorHelper;
 import com.sencha.gxt.core.client.util.DateWrapper;
 import com.sencha.gxt.core.client.util.Margins;
@@ -41,9 +51,17 @@ import com.sencha.gxt.data.shared.TreeStore;
 import com.sencha.gxt.widget.core.client.ContentPanel;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
+import com.sencha.gxt.widget.core.client.event.CollapseItemEvent;
+import com.sencha.gxt.widget.core.client.event.CollapseItemEvent.CollapseItemHandler;
+import com.sencha.gxt.widget.core.client.event.ExpandItemEvent;
+import com.sencha.gxt.widget.core.client.event.ExpandItemEvent.ExpandItemHandler;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
+import com.sencha.gxt.widget.core.client.grid.Grid;
 import com.sencha.gxt.widget.core.client.grid.HeaderGroupConfig;
+import com.sencha.gxt.widget.core.client.tree.Tree.Joint;
+import com.sencha.gxt.widget.core.client.treegrid.TreeGrid;
+import com.sencha.gxt.widget.core.client.treegrid.TreeGridView;
 
 public class PrimaveraGantt implements IsWidget {
 
@@ -59,12 +77,20 @@ public class PrimaveraGantt implements IsWidget {
 		String todayLineMain();
 	}
 
-	public interface GanttExampleResources extends ClientBundle {
+	public interface GanttResources extends ClientBundle {
+		boolean showItemLogo = false;
+
 		@Source({ "Gantt.css" })
 		GanttExampleStyle css();
+
+		@Source("folderLogo.png")
+		ImageResource folderLogo();
+
+		@Source("folderLogo.png")
+		ImageResource itemLogo();
 	}
 
-	private static final GanttExampleResources resources = GWT.create(GanttExampleResources.class);
+	private static final GanttResources resources = GWT.create(GanttResources.class);
 
 	private Gantt<Task, Dependency> gantt;
 	private static final TaskProps props = GWT.create(TaskProps.class);
@@ -145,50 +171,146 @@ public class PrimaveraGantt implements IsWidget {
 		vc.add(gantt, new VerticalLayoutData(1, 1));
 		return cp;
 	}
-	
-	private List<ZoneGeneratorInt> getZones(){
+
+	private List<ZoneGeneratorInt> getZones() {
 		// Add zones to weekends
 		ArrayList<ZoneGeneratorInt> zoneGenerators = new ArrayList<ZoneGeneratorInt>();
-//		zoneGenerators.add(new WeekendZoneGenerator()); // Create a zone every
+		// zoneGenerators.add(new WeekendZoneGenerator()); // Create a zone
+		// every
 		return zoneGenerators;
 	}
-	
+
 	// Creating the store containing the lines
-    private ListStore<Line> createLines() {
+	private ListStore<Line> createLines() {
 		StyleInjectorHelper.ensureInjected(resources.css(), true);
 
-        LineProperties lineProps = GWT.create(LineProperties.class);
-        ListStore<Line> store = new ListStore<Line>(lineProps.key());
-        // new Line(Date date,String tooltipText,String customCssStyle)
-        String customCssStyle = resources.css().todayLineMain();
-        Line line = new Line(new Date(), new Date().toString(), customCssStyle);
-        store.add(line);
-        return store;
-    }
-	
-	private static class GanttPrim extends Gantt<Task, Dependency>{
+		LineProperties lineProps = GWT.create(LineProperties.class);
+		ListStore<Line> store = new ListStore<Line>(lineProps.key());
+		// new Line(Date date,String tooltipText,String customCssStyle)
+		String customCssStyle = resources.css().todayLineMain();
+		Line line = new Line(new Date(), new Date().toString(), customCssStyle);
+		store.add(line);
+		return store;
+	}
+
+	private static class GanttPrim extends Gantt<Task, Dependency> {
 
 		public GanttPrim(TreeStore<Task> taskStore, ListStore<Dependency> dependecyStore, GanttConfig config) {
 			super(taskStore, dependecyStore, config);
 		}
-	
+
+		@Override
+		protected TreeGrid<Task> createStaticGrids(TreeStore<Task> store, ColumnModel<Task> cm) {
+			// return super.createStaticGrids(store, cm);
+			TreeGrid<Task> grid = new TreeGrid<Task>(store, cm, cm.getColumn(0));
+			grid.addStyleName("sch-grid");
+
+			TreeGridView<Task> view = new TreeGridView<Task>() {
+				protected void init(final Grid<Task> grid) {
+					vbar = false;
+					super.init(grid);
+					if (GanttResources.showItemLogo) {
+						tree.getStyle().setLeafIcon(resources.itemLogo());
+					}
+				}
+
+				@Override
+				public SafeHtml getTemplate(Task m, String id, SafeHtml text, ImageResource icon, boolean checkable,
+						Joint joint, int level) {
+					icon = icon == null ? null : resources.folderLogo();
+					return super.getTemplate(m, id, text, icon, checkable, joint, level);
+				}
+
+				@Override
+				protected void processRows(int startRow, boolean skipStripe) {
+					super.processRows(startRow, skipStripe);
+					if (ds.size() < 1) {
+						return;
+					}
+					NodeList<Element> rows = getRows();
+					for (int i = 0, len = rows.getLength(); i < len; i++) {
+						Element row = (Element) rows.getItem(i);
+						String rowHeight = String.valueOf(timePanel.getOverlapCount(i) * config.rowHeight);
+						((XElement) row).setHeight(rowHeight);
+					}
+				}
+
+				protected void calculateVBar(boolean force) {
+					if (force) {
+						resize();
+					}
+					boolean vbar = false;
+					if (force || this.vbar != vbar) {
+						this.vbar = vbar;
+						lastViewWidth = -1;
+						layout(true);
+					}
+				}
+
+				public void refresh(boolean headerToo) {
+					super.refresh(headerToo);
+					if (timePanel.isHorizontalScroll()) {
+						StringBuilder html = new StringBuilder();
+						html.append("<div class=\"sch-scrolladjust\" style=\"height:");
+						html.append(XDOM.getScrollBarWidth());
+						html.append("px\"></div>");
+						DomHelper.append(getBody(), html.toString());
+					}
+				}
+
+				protected void initElements() {
+					super.initElements();
+					scroller.getStyle().setOverflowY(Overflow.HIDDEN);
+				}
+			};
+
+			// Temp
+			view.setForceFit(true);
+			view.setAdjustForHScroll(true);
+
+			grid.setView(view);
+
+			grid.addResizeHandler(new ResizeHandler() {
+				@Override
+				public void onResize(ResizeEvent event) {
+					refresh();
+				}
+			});
+			CollapseItemHandler<Task> handler = new CollapseItemHandler<Task>() {
+				@Override
+				public void onCollapse(CollapseItemEvent<Task> event) {
+					refresh();
+				}
+			};
+			grid.addHandler(handler, CollapseItemEvent.getType());
+			ExpandItemHandler<Task> expHandler = new ExpandItemHandler<Task>() {
+				@Override
+				public void onExpand(ExpandItemEvent<Task> event) {
+					refresh();
+				}
+			};
+			grid.addHandler(expHandler, ExpandItemEvent.getType());
+			return grid;
+
+		}
+
 		@Override
 		public Date getTaskEndDate(Task task) {
 			return super.getTaskEndDate(task);
 		}
-		
+
 	}
-	
-	private void setStartEnd(){
+
+	private void setStartEnd() {
 		DateWrapper dwStart = new DateWrapper(ganttData.getDateStart()).clearTime();
 		DateWrapper dwFinish = new DateWrapper(ganttData.getDateFinish()).clearTime();
 		int delta = 4;
 		// Set start and end date.
 		String scale = ganttData.getScale();
 		if (scale.equals(ScaleConstants.DAY)) {
-			gantt.setStartEnd(dwStart.addDays(-delta).asDate(), dwFinish.addDays(delta).asDate());		
+			gantt.setStartEnd(dwStart.addDays(-delta).asDate(), dwFinish.addDays(delta).asDate());
 		} else if (scale.equals(ScaleConstants.MONTH)) {
-			gantt.setStartEnd(dwStart.addMonths(-delta).asDate(), dwFinish.addMonths(delta).asDate());		
+			gantt.setStartEnd(dwStart.addMonths(-delta).asDate(), dwFinish.addMonths(delta).asDate());
 		}
 	}
 
